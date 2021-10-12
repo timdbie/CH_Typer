bddc4c6d34ddb6094dbe594c7321e4318b2dec3b
timdb <212128@vistacollege.nl>
2021-10-12 15:02:23 +0200

    Basics (12-10-2021)
    - Lettering
    - Typing

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,5 @@
 $(() => {
+    $('.contentbox-wordsinp').show();
 
     var wordlist = ["test", "hoi", "you", "hallo", "nee"];
 
@@ -8,31 +9,45 @@ $(() => {
     w = 0;
     l = 0;
 
-    while(i <= 50) {
+
+    while(i <= 200) {
         words[i] = wordlist[Math.floor(Math.random() * 5)];
-        $(".contentbox-words").append(`<span id=w${i}>` + words[i] + "</span>")
+        $(".contentbox-words").append(`<span class=w${i}>` + words[i] + "</span>")
+        $(".w" + i).lettering();
 
         i++;
     }
 
-    $("#typer").on("keypress", (e) => {
-        if (e.which >= 97 && e.which <= 122) {           
-
+    $("#typer").on("keyup", (e) => {
+        if (e.which >= 65 && e.which <= 90) {
+            if ($("#typer").val().charAt(l) == words[w].charAt(l)){
+                $(`.w${w} > .l${l+1}`).css("color", "green")
+            } else {
+                $(`.w${w} > .l${l+1}`).css("color", "red")
+            }
+            
+            l++;
         }
-
-        if (e.keyCode == 8 || e.keyCode == 46) {
-            i--;
+ 
+        if (e.which == 8 || e.which == 46) {
+            if (l > 0){
+                $(`.w${w} > .l${l}`).css("color", "white")
+                l--;
+            }
         }
         
-        if (e.which == 13 || e.which == 32) {
+        if (e.which == 13) {
             text = $("#typer").val();
             $("#typer").val("");
 
             if (text == words[w]) {
-                $("#w" + w).css("color", "green")
+                $(".w" + w + " span").css("color", "green")
                 w++;
-                l = 0;
+            } else {
+                $(".w" + w + " span").css("color", "white")
             }
+
+            l = 0;
         }
     });
 });
