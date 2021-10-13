591c2a88b6bdd1c2250ebd1a63ff6059afe5ab74
timdb <212128@vistacollege.nl>
2021-10-13 15:04:30 +0200

    Update & Fixes (13-10-2021)
    - Fixed delay and issues
    - New keylogging system

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,13 @@
+
+
 $(() => {
-    $('.contentbox-wordsinp').show();
-
     var wordlist = ["test", "hoi", "you", "hallo", "nee"];
-
     var words = [];
 
     i = 0;
     w = 0;
     l = 0;
-
+    inputlength = 0;
 
     while(i <= 200) {
         words[i] = wordlist[Math.floor(Math.random() * 5)];
@@ -18,36 +17,44 @@ $(() => {
         i++;
     }
 
-    $("#typer").on("keyup", (e) => {
-        if (e.which >= 65 && e.which <= 90) {
-            if ($("#typer").val().charAt(l) == words[w].charAt(l)){
-                $(`.w${w} > .l${l+1}`).css("color", "green")
-            } else {
-                $(`.w${w} > .l${l+1}`).css("color", "red")
-            }
-            
-            l++;
-        }
- 
-        if (e.which == 8 || e.which == 46) {
-            if (l > 0){
-                $(`.w${w} > .l${l}`).css("color", "white")
-                l--;
-            }
-        }
+    $("#typer").attr("maxlength", `${words[0].length}`)
+
+    $("#typer").change( function() {
+        text = $("#typer").val();
+        $("#typer").val("");
         
-        if (e.which == 13) {
-            text = $("#typer").val();
-            $("#typer").val("");
+        if (text == words[w]) {
+            $(".w" + w + " span").css("color", "green")
+            $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")   
+            w++;
 
-            if (text == words[w]) {
-                $(".w" + w + " span").css("color", "green")
-                w++;
-            } else {
-                $(".w" + w + " span").css("color", "white")
-            }
-
-            l = 0;
+            $("#typer").attr("maxlength", `${words[w].length}`)
+        } else {
+            $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")
+            $(".w" + w + " span").css("color", "white")
         }
+        l = 0;
+        inputlength = 0;
+    });
+
+    $("#typer").on("input", function() {
+        if ($("#typer").val().length < inputlength) {
+            $(`.w${w} > .l${l}`).css("color", "white")
+            $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")
+            l--;
+            $(`.w${w} > .l${l}`).css("border-right", "3px solid #faf000")
+        } else {
+            if($("#typer").val().length <= words[w].length){
+                if ($("#typer").val().charAt(l) == words[w].charAt(l)) {
+                    $(`.w${w} > .l${l+1}`).css("color", "green")
+                } else {
+                    $(`.w${w} > .l${l+1}`).css("color", "red")
+                }
+                $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")
+                l++;
+                $(`.w${w} > .l${l}`).css("border-right", "3px solid #faf000")
+            }
+        }
+        inputlength = $("#typer").val().length;
     });
 });
