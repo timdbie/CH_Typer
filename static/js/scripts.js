709e6611d1f145645aa0d362b1895235b4f54d83
timdb <74897688+timdbie@users.noreply.github.com>
2021-10-14 19:39:38 +0200

    Bugfix (14-10-2021)
    - Prevents input focus bug
    - Fixed word spacing

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,100 +1,103 @@
-$(() => {
-    var wordlist = ["test", "hoi", "you", "hallo", "nee"];
-    var words = [];
+$("document").ready(function() {
+    $("#typer").css("display", "block")
+    $("#typer").focus()
+});
 
-    function startTyper () {
-        i = 0;
-        w = 0;
-        l = 0;
-        inputlength = 0;
-        toppos = 0;
-        firstline = true;
+var wordlist = ["test", "hoi", "you", "hallo", "nee"];
 
-        while(i <= 300) {        
-            words[i] = wordlist[Math.floor(Math.random() * 5)];
-            $(".contentbox-words").append(`<span class=w${i}>` + words[i] + "</span>")
-            $(".w" + i).lettering();
+function startTyper () {
+    words = [];
+    i = 0;
+    w = 0;
+    l = 0;
+    inputlength = 0;
+    toppos = 0;
+    firstline = true;
 
-            i++;
-        }
-        
-        $("#typer").attr("maxlength", `${words[0].length}`)
+    while(i <= 300) {        
+        words[i] = wordlist[Math.floor(Math.random() * 5)];
+        $(".contentbox-words").append(`<span class=w${i}>` + words[i] + "</span>")
+        $(".w" + i).lettering();
+
+        i++;
     }
-    startTyper();
+    
+    $("#typer").attr("maxlength", `${words[0].length}`)
+}
+startTyper();
 
 
-    $("#refresh").click( function() {
-        $(".contentbox-words").empty()
-        $(".contentbox-words").css("top", "0")
-        words = []
-        startTyper()
-    });
+$("#refresh").click( function() {
+    $(".contentbox-words").empty()
+    $(".contentbox-words").css("top", "0")
+    startTyper()
+});
 
-    $("html").on("keydown", (e) => {
-        if (e.which == 9) {
-            $("#refresh").focus()
-            e.preventDefault()
-        }
-    });
+$("html").on("keydown", (e) => {
+    if (e.which == 9) {
+        $("#refresh").focus()
+        e.preventDefault()
+    }
+});
 
-    $("html").on("keyup", (e) => {
-        if (e.which == 9) {
-            $("#refresh").blur()
-            $("#typer").focus()
-            e.preventDefault()
-        }
-    });
+$("html").on("keyup", (e) => {
+    if (e.which == 9) {
+        $("#refresh").blur()
+        $("#typer").focus()
+        e.preventDefault()
+    }
+});
 
 
-    $("#typer").change( function() {
-        text = $("#typer").val();
-        $("#typer").val("");
-        
-        if (text == words[w]) {
-            $(".w" + w + " span").css("color", "green")
-            $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")
 
-            oldpos = $(".w" + w).offset().top
-            w++;
-            newpos = $(".w" + w).offset().top
+$("#typer").change( function() {
+    text = $("#typer").val();
+    $("#typer").val("");
+    
+    if (text == words[w]) {
+        $(".w" + w + " span").css("color", "green")
+        $(`.w${w} > .l${l}`).css("border-right", "2px solid transparent")
 
-            if (oldpos != newpos) {
-                if (firstline == true) {
-                    firstline = false;
-                } else {
-                    toppos = toppos - 50;
-                    $(".contentbox-words").css("top", `${toppos}px`)
-                }
-            }
+        oldpos = $(".w" + w).offset().top
+        w++;
+        newpos = $(".w" + w).offset().top
 
-            $("#typer").attr("maxlength", `${words[w].length}`)
-        } else {
-            $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")
-            $(".w" + w + " span").css("color", "white")
-            $(".w" + w + " span").css("opacity", "0.2")
-        }
-        l = 0;
-        inputlength = 0;
-    });
-
-    $("#typer").on("input", function() {
-        if ($("#typer").val().length < inputlength) {
-            $(`.w${w} > .l${l}`).css("opacity", "0.2")
-            $(`.w${w} > .l${l}`).css("color", "white")
-            $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")
-            l--;
-            $(`.w${w} > .l${l}`).css("border-right", "3px solid #faf000")
-        } else {
-            if($("#typer").val().length <= words[w].length){
-                if ($("#typer").val().charAt(l) != words[w].charAt(l)) {
-                    $(`.w${w} > .l${l+1}`).css("color", "red")
-                }
-                $(`.w${w} > .l${l+1}`).css("opacity", "1")
-                $(`.w${w} > .l${l}`).css("border-right", "3px solid transparent")
-                l++;
-                $(`.w${w} > .l${l}`).css("border-right", "3px solid #faf000")
+        if (oldpos != newpos) {
+            if (firstline == true) {
+                firstline = false;
+            } else {
+                toppos = toppos - 50;
+                $(".contentbox-words").css("top", `${toppos}px`)
             }
         }
-        inputlength = $("#typer").val().length;
-    });
+
+        $("#typer").attr("maxlength", `${words[w].length}`)
+    } else {
+        $(`.w${w} > .l${l}`).css("border-right", "2px solid transparent")
+        $(".w" + w + " span").css("color", "white")
+        $(".w" + w + " span").css("opacity", "0.2")
+    }
+    l = 0;
+    inputlength = 0;
+});
+
+$("#typer").on("input", function() {
+    if ($("#typer").val().length < inputlength) {
+        $(`.w${w} > .l${l}`).css("opacity", "0.2")
+        $(`.w${w} > .l${l}`).css("color", "white")
+        $(`.w${w} > .l${l}`).css("border-right", "2px solid transparent")
+        l--;
+        $(`.w${w} > .l${l}`).css("border-right", "2px solid #faf000")
+    } else {
+        if($("#typer").val().length <= words[w].length){
+            if ($("#typer").val().charAt(l) != words[w].charAt(l)) {
+                $(`.w${w} > .l${l+1}`).css("color", "red")
+            }
+            $(`.w${w} > .l${l+1}`).css("opacity", "1")
+            $(`.w${w} > .l${l}`).css("border-right", "2px solid transparent")
+            l++;
+            $(`.w${w} > .l${l}`).css("border-right", "2px solid #faf000")
+        }
+    }
+    inputlength = $("#typer").val().length;
 });
