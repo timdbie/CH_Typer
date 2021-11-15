3f1b8e7e4254b556b77294764f6b06d3ee95cef2
timdbie <212128@vistacollege.nl>
2021-11-15 14:21:10 +0100

    -Reworked error system -New end stats

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,8 +9,10 @@ $("document").ready(function() {
         i = 0;
         w = 0;
         l = 0;
+        correctentries = 0;
         totalentries = 0;
         inputlength = 0;
+        errors = 0;
         toppos = 0;
         firstline = true;
         timerstarted = false;
@@ -53,10 +55,24 @@ $("document").ready(function() {
         $("#endingpopup").fadeTo("fast", 1)
 
         typedwords = totalentries / 5
-        wpm = typedwords / wpmval
-        wpm = Math.round(wpm)
+        grosswpm = typedwords / wpmval
+        grosswpmval = Math.round(grosswpm)
 
-        $("#endingpopup h2").html(wpm + " WPM")
+        typedcorrectwords = correctentries / 5
+        netwpm = typedcorrectwords / wpmval
+        netwpmval = Math.round(netwpm)
+
+        if(netwpmval < 0) {
+            netwpmval = 0
+        }
+
+        accuracy = Math.round((correctentries / totalentries) * 100)
+
+        $("#wpm h1").html(netwpmval)
+        $("#rawwpm").html("raw: " + grosswpmval + " WPM")
+
+        $("#accuracy h1").html(accuracy)
+        $("#entries").html("entries: " + totalentries + "/" + correctentries + "/" + errors)
         endingpopup = true
     }
 
@@ -121,29 +137,29 @@ $("document").ready(function() {
 
     function submitInput() {
         text = $("#typer").val();
-        $("#typer").val("");
-        
-        if (text == words[w]) {
-            $(".w" + w + " span").css("color", "green")
 
-            oldpos = $(".w" + w).offset().top
-            w++;
-            newpos = $(".w" + w).offset().top
-
-            if (oldpos != newpos) {
-                if (firstline == true) {
-                    firstline = false;
-                } else {
-                    toppos = toppos - 50;
-                    $(".contentbox-words").css("top", `${toppos}px`)
-                }
-            }
-
-            $("#typer").attr("maxlength", `${words[w].length}`)
-        } else {
-            $(".w" + w + " span").css("color", "rgba(255,255,255,0.20)")
-            totalentries = totalentries - l
+        if (text != words[w]) {
+            $(`.w${w} > span`).css("border-bottom", "2px solid red")
         }
+
+
+        $("#typer").val("");
+
+        oldpos = $(".w" + w).offset().top
+        w++;
+        newpos = $(".w" + w).offset().top
+
+        if (oldpos != newpos) {
+            if (firstline == true) {
+                firstline = false;
+            } else {
+                toppos = toppos - 50;
+                $(".contentbox-words").css("top", `${toppos}px`)
+            }
+        }
+
+        $("#typer").attr("maxlength", `${words[w].length}`)
+
         l = 0;
         checkChar()
         inputlength = 0;
@@ -167,23 +183,29 @@ $("document").ready(function() {
         }
     }
 
-
     $("#typer").on("input", function() {
         if (timerstarted == false) {
             startTimer()
         }
 
         if ($("#typer").val().length < inputlength) {
+            if($(`.w${w} > .l${l}`).css("color") == "rgb(255, 0, 0)") {
+                errors--;
+            } else {
+                correctentries--;
+            }
+
             $(`.w${w} > .l${l}`).css("color", "rgba(255,255,255,0.20)")
             l--;
-            totalentries--;
             checkChar()
         } else {
             if($("#typer").val().length <= words[w].length){
                 if ($("#typer").val().charAt(l) != words[w].charAt(l)) {
                     $(`.w${w} > .l${l+1}`).css("color", "red")
+                    errors++;
                 } else {
                     $(`.w${w} > .l${l+1}`).css("color", "white")
+                    correctentries++;
                 }
                 l++;
                 totalentries++;
