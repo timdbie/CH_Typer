6d3d2fbe508180ecf25803e939194e2215254239
timdb <74897688+timdbie@users.noreply.github.com>
2021-11-19 23:54:45 +0100

    Update (19-11-2021)
    -Session based authentication
    -Account generation (progress)
    -Database improvements
    -Security improvements

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const session = require("express-session")
+const MariaDBStore = require("express-session-mariadb-store")
 const db = require('./database')
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(cookieParser());
 
-app.get('/', function (req, res) {
+app.use(session({
+	secret: "123",
+	resave: true, 
+	saveUninitialized: true,
+	store: new MariaDBStore({
+		sessionTable: "tb_sessions",
+		pool: db
+	}),
+	cookie: {
+		maxAge: 365 * 24 * 60 * 60 * 1000
+	},
+	rolling : true
+}))
 
-    if (req.cookies.userToken) {
-		res.sendFile(path.join(__dirname + '/views/index.html'))
-		console.log("Account was already generated.")
-		
-    } else {
+app.get('/', function (req, res) {
+	if (!req.session.uid) {
 		function genUid() {
 			return new Promise((resolve, reject) => {
 				db.query("INSERT INTO tb_accounts (username) VALUES ('undefined')")
@@ -21,10 +32,9 @@ app.get('/', function (req, res) {
 						db.query("SELECT id FROM tb_accounts ORDER BY id DESC LIMIT 1")
 							.then((rows) => {
 								var result = rows[0].id
-								var uid = "user" + result
-								db.query("UPDATE tb_accounts SET username = '"+uid+"' WHERE id = '"+result+"'")
+								var username = "user#" + (result).toLocaleString('en-US', {minimumIntegerDigits: 5, useGrouping:false})
+								db.query("UPDATE tb_accounts SET username = '"+username+"' WHERE id = '"+result+"'")
 
-								res.cookie('userToken', result, { expires: new Date(Date.now() + 900000), httpOnly: true })
 								resolve(result);
 							})
 					});
@@ -32,10 +42,15 @@ app.get('/', function (req, res) {
 		}
 
 		genUid().then(result => {
+			req.session.uid = result
 			res.sendFile(path.join(__dirname + '/views/index.html'))
 			console.log("Account " + result + " was succesfully generated!")
 		})
-    }
+
+	} else {
+		res.sendFile(path.join(__dirname + '/views/index.html'))
+		console.log("UID is already active")
+	}
 });
 
 app.listen(3000);
